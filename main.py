11a5eff3f4da327cc28cdb0d43deb2ce4fc2c6e9
ibadia <ibad2762@gmail.com>
2019-10-04 16:13:36 +0100

    directory testing

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import os
 
 def get_file(data):
     pass
-
-if __name__ == "__main__":
-
+def change_filename(input_filename, output_filename):
     with open('csv_old_file.csv', mode='r') as file:  
         next(file)
         for line in file:
@@ -20,3 +18,11 @@ if __name__ == "__main__":
         new_file.seek(-1, os.SEEK_END)
         new_file.truncate()
     
+if __name__ == "__main__":
+
+    if not os.path.exists(os.path.join(os.getcwd(), "result")):
+        os.makedirs(os.path.join(os.getcwd(),"result"))
+    for x in os.listdir(os.getcwd()):
+        if x.find(".csv")!=-1:
+            path_file=os.path.join(os.getcwd(),"result")
+            change_filename(x,path_file+x )
